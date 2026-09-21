5ed71b7d3ca705082ce2f3b6ddf63e3c069454f8
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-21 16:13:45 -0300

    add: comentarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        //variaveis
         int dist;
         double totalg, kml;
         
+        //entrada de dados
         dist = leia.nextInt();
         totalg = leia.nextDouble();
         
+        //processamento
         kml = dist / totalg;
         
+        //saida de dados
         System.out.printf("%.3f km/l\n",kml);
     }
 }
